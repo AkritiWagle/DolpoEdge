b13448540cd03668b77a2e4842ef22ea04730642
AkritiWagle <aakritiwagle10@gmail.com>
2025-05-06 04:23:11 +0545

    Unit test added for transactions

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from store.models import Item
+from accounts.models import Customer,Vendor
+from .models import Sale, Purchase
 
-# Create your tests here.
+class TransactionTests(TestCase):
+
+    def test_sale_creation(self):
+        customer = Customer.objects.create(name="Test Customer")
+        sale = Sale.objects.create(customer=customer, grand_total=100.00)
+        self.assertIn(f"Sale ID: {sale.id}", str(sale))
+
+    def test_purchase_str(self):
+        vendor = Vendor.objects.create(name="Raw Mat Ltd")
+        purchase = Purchase.objects.create(
+            raw_material_vendor=vendor, 
+            grand_total=100.00, 
+            sub_total=100.00
+            )
+
+        self.assertIn("Purchase #", str(purchase))
